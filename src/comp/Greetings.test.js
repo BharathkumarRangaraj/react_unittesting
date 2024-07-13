934d4b90e0dee997acbd404d5d3ce97d0bf6a67e
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-07-13 11:27:46 +0530

    Update Greetings.test.js

--- src/comp/Greetings.test.js
+++ src/comp/Greetings.test.js
@@ -3,7 +3,13 @@ import Greetings from "./Grettings";
 
 describe('Greeting component',()=>{
     test('testing hello world text',()=>{
+        //arrange
         render(<Greetings/>);
+
+        //act
+        //nothing
+
+        //assert
     
         const helloworldelement=screen.getByText('Hello World',{exact:false});
         expect(helloworldelement).toBeInTheDocument();
