eb49750300f7677ef9c89b1812b3144f3d8ecb9c
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-07-13 11:25:31 +0530

    grouping the test together with test suits

--- src/comp/Greetings.test.js
+++ src/comp/Greetings.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Grettings";
 
-
-test('testing hello world text',()=>{
-    render(<Greetings/>);
-
-    const helloworldelement=screen.getByText('Hello World',{exact:false});
-    expect(helloworldelement).toBeInTheDocument();
+describe('Greeting component',()=>{
+    test('testing hello world text',()=>{
+        render(<Greetings/>);
+    
+        const helloworldelement=screen.getByText('Hello World',{exact:false});
+        expect(helloworldelement).toBeInTheDocument();
+    })
 })
+
