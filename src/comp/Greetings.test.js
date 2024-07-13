4b3bdd0e9674b0fcb19eaf5d00b12dfd481a8d43
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-07-13 12:51:18 +0530

    grouping test together with describe

--- src/comp/Greetings.test.js
+++ src/comp/Greetings.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Grettings";
+import userEvent from "@testing-library/user-event";
 
 describe('Greeting component',()=>{
     test('testing hello world text',()=>{
@@ -14,5 +15,23 @@ describe('Greeting component',()=>{
         const helloworldelement=screen.getByText('Hello World',{exact:false});
         expect(helloworldelement).toBeInTheDocument();
     })
+   test('renders good to see you if button is not clicked',()=>{
+    render(<Greetings/>);
+    const onclicktexttest=screen.getByText('good to see you',{exact:false});
+    expect(onclicktexttest).toBeInTheDocument();
+   })
+
+   test('renders changed if button is clicked',()=>{
+
+    //arrange
+    render(<Greetings/>);
+    
+    //act
+    const buttonElement=screen.getByRole('button');
+    userEvent.click(buttonElement)
+    //assert
+    const oncl=screen.getByText('changed');
+    expect(oncl).toBeInTheDocument();
+   })
 })
 
--- src/comp/Grettings.js
+++ src/comp/Grettings.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
+
 const Greetings=()=>{
+    const [changeText,SetChangeText]=useState(false);
+    const handleButtonChange=()=>{
+        SetChangeText(true);
+    }
     return(
         <div>
             <h1>Hello World!</h1>
+            <h2>{!changeText && <p>good to see you!</p>}
+            {changeText && <p>changed</p>}</h2>
+        
+            <button onClick={()=>handleButtonChange()}>Change Text!</button>
+
         </div>
     )
 }
